efed5ca19a44a6375f28f21e784063510f663b87
Pavlo Mykhalov <pmykhalov@gmail.com>
2016-07-16 12:49:26 +0300

    Add Browser component itself

---
+++ src/components/Browser.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import Explore from '../components/Explore.jsx';
+import Venues from '../components/Venues.jsx';
+
+export default class Browser extends Component {
+    render() {
+        const { pathname, query } = this.props.location;
+        const currentLocation = pathname === '/explore' ? query.near : null;
+
+        return (
+            <div className="row">
+                <div className="col-sm-4">
+                    <Explore currentLocation={currentLocation} />
+                </div>
+                <div className="col-sm-8">
+                    <Venues />
+                </div>
+            </div>
+        );
+    }
+}
