099eb518d72f43e3ca58fd06c8cc4f595be581e2
Pavlo Mykhalov <pmykhalov@gmail.com>
2016-08-14 23:11:51 +0300

    Restore production build

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
             },
         }),
         new DefinePlugin({
-            'process.env.NODE_ENV': 'production',
+            'process.env.NODE_ENV': '"production"',
         }),
     ]);
     devtool = null;
